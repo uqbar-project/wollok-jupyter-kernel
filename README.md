08af2cf7d95612ec0e0258c534fd4f23e758f580
Fernando Dodino <fernando.dodino@gmail.com>
2025-04-11 10:00:41 -0300

    Add PoC

---
+++ README.md
@@ -0,0 +1,30 @@
+## Wollok Kernel for Jupyter notebook
+
+`wollok_kernel` is a Jupyter kernel implementation, started from [a wrapper kernel](http://jupyter-client.readthedocs.io/en/latest/wrapperkernels.html).
+
+## Installation
+
+### From PyPI
+
+To install `wollok_kernel` from PyPI:
+
+```bash
+pip install wollok_kernel
+```
+    
+### From Git using Conda
+
+To install `wollok_kernel` from git into a Conda environment:
+
+```bash
+git clone https://github.com/jupyter/wollok_kernel
+cd wollok_kernel
+conda create -n ker jupyter
+conda activate ker
+pip install .
+```
+
+## Using Wollok kernel
+
+- **Notebook**: The *New* menu in the notebook should show an option for a Wollok notebook.
+- **Console frontends**: To use it with the console frontends, add ``--kernel wollok`` to their command line arguments.
---
+++ wollok_kernel/kernel.py
@@ -0,0 +1,26 @@
+from ipykernel.kernelbase import Kernel
+
+class WollokKernel(Kernel):
+    implementation = 'Wollok'
+    implementation_version = '1.0'
+    language = 'no-op'
+    language_version = '0.1'
+    language_info = {
+        'name': 'wollok',
+        'mimetype': 'text/plain',
+        'file_extension': '.wlk',
+    }
+    banner = "Wollok >>> kernel"
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None,
+                   allow_stdin=False):
+        if not silent:
+            stream_content = {'name': 'stdout', 'text': code}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        return {'status': 'ok',
+                # The base class increments the execution count
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+               }
