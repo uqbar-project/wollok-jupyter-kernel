ddf456243e83f1617cbdd0c4637f15ed8f283ab5
Fernando Dodino <fernando.dodino@gmail.com>
2025-04-29 19:46:03 -0300

    Pythonmonkey PoC (#1)
    * Pythonmonkey PoC
    
    * Using python function - require still does not work
    
    * PoC: call eval with dates
    
    * Custom kernel test
    
    * using 3.11 instead of pypi
    
    * Exporting node libraries to wheel
    
    * Fix test
    
    * Fix PEP
    
    * Remove print
    
    * trying to fix ci
    
    * fix check release for ci
    
    * Using new version
    
    * using version 1.3
    
    * Trying to fix check release
    
    * check release - ls
    
    * add sdist forced include
    
    * Testing jupyter pre-clean method
    
    * Forcing npm build
    
    * Copying node_modules folder
    
    * testing node_modules for check_release
    
    * test 1
    
    * test 2
    
    * test 3
    
    * Ignore check-release on PR
    
    * Delete manifest (not needed)
    
    * Formatting files with black
    
    * Fix imports
    
    * Fix #2 -> add type hints
    
    * Add Python profile
    
    * Developer instructions moved to wiki

--- README.md
+++ README.md
@@ -6,27 +6,10 @@
 
 `wollok_kernel` is a Jupyter kernel implementation, started from [a wrapper kernel](http://jupyter-client.readthedocs.io/en/latest/wrapperkernels.html).
 
-## Installation
+## Contribution
 
-### From PyPI
+If you want to collaborate, follow the [developer instructions](https://github.com/uqbar-project/wollok-jupyter-kernel/wiki/Developer-environment).
 
-To install `wollok_kernel` from PyPI:
-
-```bash
-pip install wollok_kernel
-```
-    
-### From Git using Conda
-
-To install `wollok_kernel` from git into a Conda environment:
-
-```bash
-git clone https://github.com/jupyter/wollok_kernel
-cd wollok_kernel
-conda create -n ker jupyter
-conda activate ker
-pip install .
-```
 
 ## Using Wollok kernel
 
--- wollok_kernel/kernel.py
+++ wollok_kernel/kernel.py
@@ -1,26 +1,34 @@
 from ipykernel.kernelbase import Kernel
+from wollok_kernel.wollok_ts.library import fn_test
+
 
 class WollokKernel(Kernel):
-    implementation = 'Wollok'
-    implementation_version = '1.0'
-    language = 'no-op'
-    language_version = '0.1'
+    implementation = "Wollok"
+    implementation_version = "1.2"
+    language = "wollok"
+    language_version = "3.3.1"
     language_info = {
-        'name': 'wollok',
-        'mimetype': 'text/plain',
-        'file_extension': '.wlk',
+        "name": "wollok",
+        "mimetype": "text/plain",
+        "file_extension": ".wlk",
     }
     banner = "Wollok >>> kernel"
 
-    def do_execute(self, code, silent, store_history=True, user_expressions=None,
-                   allow_stdin=False):
+    def do_execute(
+        self, code: str, silent: bool, store_history=True, user_expressions=None, allow_stdin=False
+    ):
         if not silent:
-            stream_content = {'name': 'stdout', 'text': code}
-            self.send_response(self.iopub_socket, 'stream', stream_content)
+            result = fn_test(code)
+            stream_content = {"name": "stdout", "text": result, "data": result}
+            self.send_response(self.iopub_socket, "stream", stream_content)
 
-        return {'status': 'ok',
-                # The base class increments the execution count
-                'execution_count': self.execution_count,
-                'payload': [],
-                'user_expressions': {},
-               }
+        return {
+            "status": "ok",
+            # The base class increments the execution count
+            "execution_count": self.execution_count,
+            "payload": [],
+            "user_expressions": {},
+        }
+
+    def do_apply(self, content: str, bufs, msg_id: str, reply_metadata):
+        return {"status": "ok", "started": True}
