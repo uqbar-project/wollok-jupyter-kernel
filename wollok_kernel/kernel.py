1801c40f055c50a733a922dee0f60da00828016e
Fernando Dodino <fernando.dodino@gmail.com>
2025-04-28 23:12:38 -0300

    Formatting files with black

--- wollok_kernel/kernel.py
+++ wollok_kernel/kernel.py
@@ -1,27 +1,30 @@
 from ipykernel.kernelbase import Kernel
 from wollok_kernel.wollok_ts.library import fn_is_odd_python
 
+
 class WollokKernel(Kernel):
-    implementation = 'Wollok'
-    implementation_version = '1.0'
-    language = 'no-op'
-    language_version = '0.1'
+    implementation = "Wollok"
+    implementation_version = "1.0"
+    language = "no-op"
+    language_version = "0.1"
     language_info = {
-        'name': 'wollok',
-        'mimetype': 'text/plain',
-        'file_extension': '.wlk',
+        "name": "wollok",
+        "mimetype": "text/plain",
+        "file_extension": ".wlk",
     }
     banner = "Wollok >>> kernel"
 
-    def do_execute(self, code, silent, store_history=True, user_expressions=None,
-                   allow_stdin=False):
+    def do_execute(
+        self, code, silent, store_history=True, user_expressions=None, allow_stdin=False
+    ):
         if not silent:
-            stream_content = {'name': 'stdout', 'text': fn_is_odd_python(code)}
-            self.send_response(self.iopub_socket, 'stream', stream_content)
+            stream_content = {"name": "stdout", "text": fn_is_odd_python(code)}
+            self.send_response(self.iopub_socket, "stream", stream_content)
 
-        return {'status': 'ok',
-                # The base class increments the execution count
-                'execution_count': self.execution_count,
-                'payload': [],
-                'user_expressions': {},
-               }
+        return {
+            "status": "ok",
+            # The base class increments the execution count
+            "execution_count": self.execution_count,
+            "payload": [],
+            "user_expressions": {},
+        }
