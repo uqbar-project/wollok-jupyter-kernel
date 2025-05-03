4302a7af02868d16592b6e2ee8ce3797b8b9ac09
Fernando Dodino <fernando.dodino@gmail.com>
2025-05-03 01:17:21 -0300

    Fix check release (#3)
    * Test with npm i inside hatch build
    
    * Removing node install as previous step (should use the one from hatch)
    
    * Testing with another dir
    
    * Using latest python version
    
    * Forcing Python last version
    
    * Add workaround for error in Windows
    
    * Fix yml
    
    * Fix build in Windows
    
    * Ignoring npm install in hatch build for Windows
    
    * Using generic solution
    
    * Enhancing npm install recursively in Win
    
    * Restricting to look for package.json only in wollok_kernel
    
    * Enhancing dirs with package.json in script

--- hatch_build.py
+++ hatch_build.py
@@ -1,7 +1,8 @@
 import os
 import sys
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
-
+import subprocess
+import platform
 
 import argparse
 import json
@@ -23,6 +24,12 @@ class CustomHook(BuildHookInterface):
         sys.path.insert(0, here)
         prefix = os.path.join(here, 'data_kernelspec')
 
+        if platform.system() != "Windows":
+            self.find_and_install_npm_dependencies(start_dir=here)
+        else:
+            print("Skipping node dependencies installation on Windows")
+
+
         with TemporaryDirectory() as td:
             os.chmod(td, 0o755) # Starts off as 700, not user readable
             with open(os.path.join(td, 'kernel.json'), 'w') as f:
@@ -38,3 +45,17 @@ class CustomHook(BuildHookInterface):
                     print("Custom logo files not found. Default logos will be used.")
 
             KernelSpecManager().install_kernel_spec(td, 'wollok', user=False, prefix=prefix)
+
+
+    def find_and_install_npm_dependencies(self, start_dir="."):
+        """Find and install npm dependencies for every package.json files in subfolders"""
+        print("Installing node dependencies")
+        subprocess.run(["pwd"])
+        for root, dirs, files in os.walk(start_dir):
+            if "package.json" in files:
+                print(f"package.json found in: {root}")
+                try:
+                    subprocess.run(["npm", "install", "--verbose"], cwd=root, check=True)
+                    print(f"✅ npm install completed in {root}\n")
+                except subprocess.CalledProcessError as e:
+                    print(f"❌ Error executing npm install in {root}: {e}\n")
